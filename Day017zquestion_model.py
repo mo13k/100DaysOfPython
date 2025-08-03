b2175da84e1ef47e2c77bbb420f6f657649d6050
mo13k <99134823+mo13k@users.noreply.github.com>
2025-08-02 23:51:17 -0400

    Update Day017zquestion_model.py

--- Day017zquestion_model.py
+++ Day017zquestion_model.py
@@ -1,5 +1,6 @@
 class Question:
 
-    def __init__(self, qtext, qanswer):
+    def __init__(self, qtext, qanswer, options=None):
         self.text = qtext
         self.answer = qanswer
+        self.options = options
